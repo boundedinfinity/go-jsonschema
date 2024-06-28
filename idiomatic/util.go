a81c07209b65621ee78cf84a0b9c51e39380d82c
Brad Babb <brad_babb@cable.comcast.com>
2024-06-28 15:04:38 -0500

    updates

--- idiomatic/util.go
+++ idiomatic/util.go
@@ -1,77 +1,95 @@
 package idiomatic
 
 import (
-	"github.com/boundedinfinity/go-jsonschema/model"
+	"github.com/boundedinfinity/go-commoner/errorer"
 	"golang.org/x/exp/constraints"
 )
+
+func copySchemaMap(m map[string]JsonSchema) map[string]JsonSchema {
+	results := map[string]JsonSchema{}
+
+	for k, v := range m {
+		results[k] = v.Copy()
+	}
+
+	return results
+}
+
+func copySchemaArray(elems []JsonSchema) []JsonSchema {
+	var results []JsonSchema
+
+	for _, elem := range elems {
+		results = append(results, elem.Copy())
+	}
+
+	return results
+}
 
 type number interface {
 	constraints.Integer | constraints.Float
 }
 
-func validateMaxMin[T number](max, min T) error {
-	if max < min {
-		return model.ErrJsonSchemaNumberInvalidMaxMinv(max, min)
-	}
+func validateMaxMin[T number](max, min T, err *errorer.Errorer) error {
+	// if max < min {
+	// 	return model.ErrJsonSchemaNumberInvalidMaxMinv(max, min)
+	// }
 
 	return nil
 }
 
-func validateMultipleOf[T number](v int) error {
+func validateMultipleOf[T number](v T, err *errorer.Errorer) error {
 	if v < 0 {
-		return model.ErrJsonSchemaNumberMultipleOfInvalidv(v)
-	}
-
-// 	if min < 0 {
-// 		return model.ErrJsonSchemaArrayMinContainsInvalidv("%v", min)
-// 	}
-
-func validateMaxMinContains[T number](max, min T) error {
-	if max < 0 {
-		return model.ErrJsonSchemaArrayMaxContainsInvalidv(max)
-	}
-
-	if min < 0 {
-		return model.ErrJsonSchemaArrayMinContainsInvalidv(min)
-	}
-
-	if max < min {
-		return model.ErrJsonSchemaArrayInvalidMaxMinv(min, max)
-	}
-
-// 	if min < 0 {
-// 		return model.ErrJsonSchemaStringMinLengthv("%v", min)
-// 	}
-
-func validateMaxMinLength[T number](max, min T) error {
-	if max < 0 {
-		return model.ErrJsonSchemaStringMaxLengthv(max)
-	}
-
-	if min < 0 {
-		return model.ErrJsonSchemaStringMinLengthv(min)
-	}
-
-	if max < min {
-		return model.ErrJsonSchemaStringMaxMinLengthv(max, min)
-	}
-
-// 	if min < 0 {
-// 		return model.ErrJsonSchemaObjectMinPropertiesv("%v", min)
-// 	}
-
-func validateMaxMinProperties[T number](max, min T) error {
-	if max < 0 {
-		return model.ErrJsonSchemaObjectMaxPropertiesv(max)
-	}
-
-	if min < 0 {
-		return model.ErrJsonSchemaObjectMinPropertiesv(min)
-	}
-
-	if max < min {
-		return model.ErrJsonSchemaObjectMaxMinPropertiesv(max, min)
+		return err.WithValue(v)
 	}
 
 	return nil
 }
+
+func validateMaxMinContains[T number](max, min T, err *errorer.Errorer) error {
+	// if max < 0 {
+	// 	return model.ErrJsonSchemaArrayMaxContainsInvalidv(max)
+	// }
+
+	// if min < 0 {
+	// 	return model.ErrJsonSchemaArrayMinContainsInvalidv(min)
+	// }
+
+	// if max < min {
+	// 	return model.ErrJsonSchemaArrayInvalidMaxMinv(min, max)
+	// }
+
+	return nil
+}
+
+// func validateMaxMinLength[T number](max, min T) error {
+// 	if max < 0 {
+// 		return model.ErrJsonSchemaStringMaxLengthv(max)
+// 	}
+
+// 	if min < 0 {
+// 		return model.ErrJsonSchemaStringMinLengthv(min)
+// 	}
+
+// 	if max < min {
+// 		return model.ErrJsonSchemaStringMaxMinLengthv(max, min)
+// 	}
+
+// // 	if min < 0 {
+// // 		return model.ErrJsonSchemaObjectMinPropertiesv("%v", min)
+// // 	}
+
+// func validateMaxMinProperties[T number](max, min T) error {
+// 	if max < 0 {
+// 		return model.ErrJsonSchemaObjectMaxPropertiesv(max)
+// 	}
+
+// 	if min < 0 {
+// 		return model.ErrJsonSchemaObjectMinPropertiesv(min)
+// 	}
+
+// 	if max < min {
+// 		return model.ErrJsonSchemaObjectMaxMinPropertiesv(max, min)
+// 	}
+
+// 	return nil
+// }
